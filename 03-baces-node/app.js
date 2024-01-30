e8ffc1315ccb1667431d8db8bdd855b250f4a930
Jasubi Piñeyro <gameportal7@hotmail.com>
2024-01-29 18:12:25 -0600

    Introduccion a Yargs para configurar flags en nuestra app de node

--- 03-baces-node/app.js
+++ 03-baces-node/app.js
@@ -1,21 +1,38 @@
 const { crearArchivoTabla } = require ("./helpers/multiplicar");
+const argv = require("yargs")
+  .option('b',{
+    alias:"base",
+    type:"number",
+    demandOption: true
+  })
+  .option("l",{
+    alias:"list",
+    type:"boolean",
+    default: false,
+    demandOption: false
+  })
+  .check((argv, options) =>{
+    if(isNaN(argv.b)){
+      throw "La base tiene que ser un numero"
+    }
 
-console.clear();
+    return true
+  })
+  .argv;
 
-const [ , , tabla = "base=1"] = process.argv;
-const [ , base = 1] = tabla.split("=")
-
-
-
-
-
-
-
-
-
-
-// const base = 7;
-
-crearArchivoTabla( base )
+  
+  
+  console.clear();
+  
+  console.log(argv)
+  
+  
+  crearArchivoTabla( argv.b, argv.l )
   .then( nombreArchivo => console.log(nombreArchivo, "Creado"))
   .catch(err => console.log(err));
+
+
+
+  
+  // const [ , base = 1] = tabla.split("=")
+  // const [ , , tabla = "base=1"] = process.argv;
--- 03-baces-node/helpers/multiplicar.js
+++ 03-baces-node/helpers/multiplicar.js
@@ -1,17 +1,20 @@
 const fs = require("fs")
 
-const crearArchivoTabla = async ( base = 5 ) =>{
+const crearArchivoTabla = async ( base = 5, list ) =>{
   try{
     let salida = "";
     const header = `
-  ======================
-      Tabla del ${ base }
-  ======================
+======================
+    Tabla del ${ base }
+======================
   `
   
     for(let i = 1; i <= 10 ; i++){
       salida += `${ base } x ${i} = ${base * i}\n`;
-      console.log( salida )
+    }
+    
+    if(list){
+      console.log(salida)
     }
     
     fs.writeFileSync(`tabla-${ base }.txt`, header + salida)
